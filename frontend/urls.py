d194075c1ef83707b073ff0e18c76c9259862aa8
Antonio Simental <antoniosimental@WorkMacBook.local>
2023-08-05 16:27:30 -0700

    This is from ch 5

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from .views import index
 
 urlpatterns = [
-    path('', view = index)
+    path('', view = index),
+    path('join', view = index),
+    path('create', view = index)
+
 ]
